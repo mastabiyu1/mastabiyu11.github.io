ce36fe2a467e088e1000715b173fc300990fd0b4
mastabiyu <55817560+mastabiyu@users.noreply.github.com>
2021-03-23 17:51:37 -0400

    OOP

--- labW1D1/app.js
+++ labW1D1/app.js
@@ -30,26 +30,44 @@ function add(...args) {
 	0 - 59   NC
 */
 
-	function computeGrade(grades) {
-		let avg=0;
-		let=sum=0;
-		for(let i=0;i<grades.length;i++){
-			  sum +=grades[i];
-		}
-		avg=sum/grades.length;
+	// function computeGrade(grades) {
+	// 	let avg=0;
+	// 	let=sum=0;
+	// 	for(let i=0;i<grades.length;i++){
+	// 		  sum +=grades[i];
+	// 	}
+	// 	avg=sum/grades.length;
 		 
-		if(avg > 89 && avg <= 100){
-			return "A";
-		}else if(avg > 79 && avg <= 89){
-			return "B";
-		}else if(avg > 69 && avg <= 79){
-			return "C";
-		}else if(avg > 60 && avg <= 69){
-			return "D";
+	// 	if(avg > 89 && avg <= 100){
+	// 		return "A";
+	// 	}else if(avg > 79 && avg <= 89){
+	// 		return "B";
+	// 	}else if(avg > 69 && avg <= 79){
+	// 		return "C";
+	// 	}else if(avg > 60 && avg <= 69){
+	// 		return "D";
+	// 	}else{
+	// 		return "NC";
+	// 	}
+	// 	  return;
+	function computeGrade(grades) {
+		let sum=0;
+		let avg;
+		for(let i=0;i<grades.length;i++){
+		sum+=grades[i]
+		avg=sum/grades.length;
+		}
+		if(avg>=90 && avg <=100){
+		return "A";
+		}else if(avg>=80 && avg<=89){
+		return "B";
+		}else if(avg>=70 && avg<=79){
+		return "C";
+		}else if(avg>=60 && avg <=69){
+		return "D";
 		}else{
-			return "NC";
+		return "NC"
 		}
-		  return;
-		}
+	   }		
 
 
--- labW1D2/app.js
+++ labW1D2/app.js
@@ -1,4 +1,30 @@
-function load() {
-	let id = document.getElementById("result");
-	id.innerHTML = "Lab W1D2";
-}
+/* eslint-disable require-jsdoc */
+/* eslint-disable no-unused-vars */
+"use strict";
+
+// See tests.js for behavior
+function nameString(obj) {
+  
+  
+        
+        if (obj === undefined) {
+        return "no object passed";
+        } else if (obj === null) {
+        return "null argument";
+        } else {
+        if (Object.keys(obj).length === 0) {
+        return "object has no properties";
+        } else {
+        let keys = [];
+        let result;
+        for (let key in obj) {
+        keys.push(key); //[a,z,m]
+        result = keys.sort().join("."); //[a,m,z]
+        //"a.m.z" 
+        }
+        return result;
+        }
+        }
+        
+        }
+
