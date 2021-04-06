d541dbd52a83f7faa19d2bbba96d5739e1ebc11b
mastabiyu <55817560+mastabiyu@users.noreply.github.com>
2021-04-06 03:10:59 -0500

    OOPLab10

--- newLecture/LabAsig10/lab10q1.js
+++ newLecture/LabAsig10/lab10q1.js
@@ -3,17 +3,20 @@ function askPassword(ok, fail) {
     let password = prompt("Password?", '');
     if (password == "rockstar") ok();
     else fail();
-    }
-    let user = {
+  }
+  
+  let user = {
     name: 'John',
-    loginOk() {
-    alert(`${this.name} logged in`);
-    },
-    loginFail() {
-    alert(`${this.name} failed to log in`);
-    },
-    };
+  
+    login(result) {
+      alert( this.name + (result ? ' logged in' : ' failed to log in') );
+    }
+  };
+  
+  //askPassword(?, ?); // ?
+
     askPassword(()=>user.loginOk(true), ()=>user.loginFail(false));
     askPassword(user.loginOk.bind(user,true),user.loginFail.bind(user,false));
     askPassword(()=>user.loginOk.call(user,true),()=>user.loginFail.call(user,false));
     askPassword(()=>user.loginOk.apply(user,true),()=>user.loginFail.apply(user,false))
+/////////////////////////////////////////////////////////////////////////////////////////////
